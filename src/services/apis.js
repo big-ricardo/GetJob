33728a689300ef3d274f6960e9c573a61ad4a0d0
luiss1569 <rcsrcs1569@gmail.com>
2019-10-01 15:28:10 -0300

    Implementação chatt

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const api  = axios.create({
     // baseURL: 'http://localhost:3333',
-    baseURL: 'http://getjobserver.herokuapp.com',
+    baseURL: 'https://getjobserver.herokuapp.com',
 })
 
 export default api
