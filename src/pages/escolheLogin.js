5d22e094c549c314762b0ca235f07dd21506d518
luiss1569 <rcsrcs1569@gmail.com>
2019-10-15 18:55:27 -0300

    Ajustes Responsividade

--- src/pages/escolheLogin.js
+++ src/pages/escolheLogin.js
@@ -61,97 +61,89 @@ export default function Login({ history }) {
     return (
 
         <body>
-           
-             <nav> 
-                 <div className="inicio">
-                     <img src={log} alt="tindev" className="logoo" /><img src={logo} alt="tindev" className="logo" />
-                 </div>
-                 <ul>
-                     <li>
-                        
-                    </li>
-                </ul>
-             </nav>
-         
+            <nav>
+                <div className="inicio">
+                    <img src={log} alt="tindev" className="logoo" /><img src={logo} alt="tindev" className="logo" />
+                </div>
+            </nav><br /><br />
+
             <div className="container">
-            <Container>
-                <Row>
-                    <Col xs={{size: 0}} sm={{ size: 12 }} md={{ size: 12 }} lg={{ size: 12 }} xl={{ size: 12 }}>
-                        <div className="carrosel">
-                            <UncontrolledCarousel items={items} className="image" />
+                <Container>
+                    <Row>
+                        <Col xs={{ size: 0 }} sm={{ size: 12 }} md={{ size: 12 }} lg={{ size: 12 }} xl={{ size: 12 }}>
+                            <div className="carrosel">
+                                <UncontrolledCarousel items={items} className="image" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+                <br />
+
+                <center><h1>Quem somos?</h1></center>
+                <p>O getJob() é uma ampla plataforma voltada para a área de Informática que tem como objetivo ajudar os profissionais da
+                              área a encontrar as melhores vagas. <br />Na nossa plataforma você pode:</p>
+
+
+
+                <div className="contas">
+                    <p className="tipo">Escolha a conta ideal para você!</p>
+                    <div className='collum'>
+
+                        <div class=" texto1">
+                            <h2>Conta Pessoal</h2>
+                            <h3>Conta voltada para pessoas em busca de empregos ou estágios!</h3>
+                            <a className="efeito" href="logindev"><img src={pesso} alt="logo" width="200px" /></a>
                         </div>
-                    </Col>
-                </Row>
-            </Container>
-            <br/>
-             
-                <center><h1>Quem somos?</h1></center>  
-                          <p>O getJob() é uma ampla plataforma voltada para a área de Informática que tem como objetivo ajudar os profissionais da 
-                              área a encontrar as melhores vagas. <br/>Na nossa plataforma você pode:</p>
-                    <div className="qs">
-                        
-                        <div className="lista">
-                          <ul>
-                            <li>Entrar como Úsuario com seu perfil do GitHub</li>
-                            <li>Visualizar vagas de <strong>empregos</strong> ou <strong>estágios</strong></li>
-                            <li>Mostrar interesse nas vagas desejadas</li>
-                          </ul>
+                        <div class=" texto">
+                            <h2>Conta Comercial</h2>
+                            <h3>Conta voltada para empresas que dejesam divulgar suas vagas!</h3>
+                            <a className="efeito" href="loginemp">
+                                <img src={comerci} alt="logo" width="200px" />
+                            </a>
                         </div>
-                        <center><img className="cont" src={contrato} alt="Cont"/></center>
-                        <div className="lista1">
-                          <ul>
-                            <li>Criar o cadastro da sua Empresa</li>
-                            <li>Criar e divulgar vagas</li>
-                            <li>Escolher o candidato ideal e conversar com ele pelo chat dentro da plataforma</li>
-                          </ul>
-                          </div>
-                        
-                    </div>
-                    
-                
-               <div className="contas"> 
-               <p className="tipo">Escolha a conta ideal para você!</p>
-                <div className='collum'>
-                    
-                    <div class=" texto1">
-                        
-                        <h2>Conta Pessoal</h2>
-                        <a className="efeito" href="logindev"><img src={pesso} alt="logo" width="200px" /></a>
-                        <h3>Conta voltada para pessoas em busca de empregos ou estágios!</h3>
-                    </div>
-                    <div class=" texto">
-                        
-                        <h2>Conta Comercial</h2>
-                        <a className="efeito" href="loginemp">
-                            <img src={comerci} alt="logo" width="200px" />
-                        </a>
-                        <h3>Conta voltada para empresas que dejesam divulgar suas vagas!</h3>
                     </div>
                 </div>
+                <div className="qs">
+                    <div className="lista">
+                        <ul>
+                            <li className="efeito">Entrar como Úsuario com seu perfil do GitHub</li>
+                            <li className="efeito">Visualizar vagas de <strong>empregos</strong> ou <strong>estágios</strong></li>
+                            <li className="efeito">Mostrar interesse nas vagas desejadas</li>
+                        </ul>
+                    </div>
+                    <center><img className="cont" src={contrato} alt="Cont" /></center>
+                    <div className="lista1">
+                        <ul>
+                            <li className="efeito">Criar o cadastro da sua Empresa</li>
+                            <li className="efeito">Criar e divulgar vagas</li>
+                            <li >Escolher o candidato ideal e conversar com ele pelo chat dentro da plataforma</li>
+                        </ul>
+                    </div>
+
                 </div>
                 <script src='./acoes.js' />
                 <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
             </div>
             <footer className="rodape">
-               <div className="rods">
-                  <p className="rod1">Desenvolvedores:<br/>Luís Ricardo Albano Santos<br/>Açucena Lemes A. Silva<br/>Luan Lucas da Silva</p>
-                  <p className="rod">  Redes Sociais:<br/>
-                  <a  href="#"><img className="redes2" src={git} alt="git"/></a>
-                  <a  href="#"><img className="redes1" src={gmail} alt="git"/></a>
-                  <a  href="#"><img className="redes2" src={insta} alt="git"/></a>
-                  
-                  </p>
-                  <p className="rod">Apps:<br/>
-                  <a  href="#"><img className="redes2" src={android} alt="git"/></a>
-                  <a  href="#"><img className="redes2" src={ios} alt="git"/></a>
-                  </p>
+                <div className="rods">
+                    <p className="rod1 rod"><strong>Desenvolvedores:</strong><br />Luís Ricardo Albano Santos<br />Açucena Lemes A. Silva<br />Luan Lucas da Silva</p>
+                    <p className="rod">  Redes Sociais:<br />
+                        <a href="#"><img className="redes2" src={git} alt="git" /></a>
+                        <a href="#"><img className="redes1" src={gmail} alt="git" /></a>
+                        <a href="#"><img className="redes2" src={insta} alt="git" /></a>
+
+                    </p>
+
+                    <p className="rod">Apps:<br />
+                        <a href=" "><img className="redes2" src={android} alt="git" /></a>
+                        <a href=" "><img className="redes2" src={ios} alt="git" /></a>
+                    </p>
                 </div>
-               <p className="final">2019 - getJob - Todos os direitos reservados.</p>  
-               
-           </footer> 
-           
+                <p className="final">2019 - getJob - Todos os direitos reservados.</p>
+            </footer>
+
+
         </body>
-        
     );
 }
 
